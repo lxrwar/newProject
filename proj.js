1f944f4743454b7cfdad7366f361a5cacf51873a
lxrwar <lxrwar@gmail.com>
2023-05-26 13:17:31 +0300

    created file

---
+++ proj.js
@@ -0,0 +1,13 @@
+let name = 'Odie Willms';
+let adress = 'Security';
+
+function onPrint(){
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element); //TODO:hello world
+    }
+}
+
+const colorWhite = '#fbf999';
+console.log(name);
+
